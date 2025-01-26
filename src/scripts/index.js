1da898ac5e7f5c30485ce58a1a06aa8587f619f9
Alexander Vestbo Hardeland <alexanderhardeland@gmail.com>
2025-01-26 07:50:30 +0100

    Adds a router file to fetch calls from the internet, then output the calls on the express server running in the browser from index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,26 @@
 // Simple express server from the book/express docs
-const express = require('express');
+import { routeHello, routeAPINames } from "./route.js";
+import express from "express";
+
+//Server on port 3000
 const server = express();
 const port = 3000;
 
+//We create one endpoint called /hello
 server.get('/hello', function(req, res){
-		res.send('Hello World, testing to see if live reload is enable');		
+		const response = routeHello(req, res);
+		res.send(response);		
+});
+
+//Create one endpoint on /api/name" 
+server.get("/api/name", async function (req, res) {
+		let response;
+		try {
+				response = await routeAPINames(req, res);
+		} catch (error) {
+						console.log(error);
+		}
+		res.send(response);
 });
 
 server.listen(port, function () {
