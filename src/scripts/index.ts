0375cf45906c1209a412cd3b0b97fd5fa4cedb57
Alexander Vestbo Hardeland <alexanderhardeland@gmail.com>
2025-02-03 11:22:45 +0100

    Adds two types and one interface in custom file and rewrote route and the index files to use TS types

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,28 +1,37 @@
 // Simple express server from the book/express docs
-import { routeHello, routeAPINames } from "./route.js";
-import express from "express";
+import { routeHello, routeAPINames, routeWeather } from "./route.js";
+import express, { Request, Response } from "express";
 
 //Server on port 3000
 const server = express();
 const port = 3000;
 
 //We create one endpoint called /hello
-server.get('/hello', function(req, res){
-		const response = routeHello(req, res);
+server.get('/hello', function(_req: Request, res: Response): void {
+		const response = routeHello();
 		res.send(response);		
 });
 
-//Create one endpoint on /api/name" 
-server.get("/api/name", async function (req, res) {
-		let response;
+//Create endpoint on /api/name" 
+server.get("/api/name", async function (_req: Request, res: Response): Promise<void> {
+		let response: string;
 		try {
-				response = await routeAPINames(req, res);
+				response = await routeAPINames();
+				res.send(response);
 		} catch (error) {
 						console.log(error);
+				}
 		}
-		res.send(response);
-});
+);
 
-server.listen(port, function () {
+server.get(
+		"/api/weather/:zipcode",
+		function (req: Request, res: Response): void {
+				const response = routeWeather({ zipcode: req.params.zipcode });
+				res.send(response);
+		}
+);
+
+server.listen(port, function (): void {
 		console.log('Listining on ' + port);
 });
--- src/scripts/route.ts
+++ src/scripts/route.ts
@@ -1,19 +1,30 @@
 import fetch from "node-fetch";
 
-const routeHello = () => "On port /hello";
+const routeHello = (): string => "On port /hello";
 
-const routeAPINames = async () => {
+const routeAPINames = async (): Promise<string> => {
 		const url = "https://www.usemodernfullstack.dev/api/v1/users";
-		let data;
+		let data: responseItemType[];
 
 		try {
 				const response = await fetch(url);
-				data = await response.json();
+				data = (await response.json()) as responseItemType[];
 		} catch (error) {
 						return error;
 		}
 
 		const names = data.map((item) => `id: ${item.id}, name: ${item.name}`).join("<br>");
 		return names;
+};
+
+const routeWeather = (query: WeatherQueryInterface): WeatherDetailType => 
+		queryWeatherData(query);
+
+const queryWeatherData = (query: WeatherQueryInterface): WeatherDetailType => {
+		return {
+				zipcode: query.zipcode,
+				weather: "sunny",
+				temp: 35
+		};
 };
 export { routeHello, routeAPINames };
