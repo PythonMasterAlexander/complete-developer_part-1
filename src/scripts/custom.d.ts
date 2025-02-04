31ce10fd2b5178bd119d99dd1885eeba00a71edd
Alexander Vestbo Hardeland <alexanderhardeland@gmail.com>
2025-02-04 07:31:07 +0100

    Documented the custom file's scope and intent.

--- src/scripts/custom.d.ts
+++ src/scripts/custom.d.ts
@@ -1,3 +1,14 @@
+/* To use custom TYPES universally in your project(directory) you can define
+ them in a type declaration files like this one which uses .d.ts file
+ extension.
+
+	Follow a simple rule of thumb: Every time we use a object, we should consider
+	using a custom type or interface. 
+
+	If the object is a function parameter, we will create a custom interface. If we 
+	use this particular object more than once, we'll create a custom type.
+*/
+
 type responseItemType = {
 		id: string;
 		name: string;
