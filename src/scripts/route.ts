e10000360b7fbede2c2bbe7b1cd4b857828dc2fc
Alexander Vestbo Hardeland <alexanderhardeland@gmail.com>
2025-02-04 08:35:45 +0100

    Adds more documentation about type annotations in route and added whitespace between comments in index.

--- src/scripts/route.ts
+++ src/scripts/route.ts
@@ -2,12 +2,19 @@ import fetch from "node-fetch";
 
 const routeHello = (): string => "On port /hello";
 
+// Annotate the function's parameters and return type 
 const routeAPINames = async (): Promise<string> => {
 		const url = "https://www.usemodernfullstack.dev/api/v1/users";
 		let data: responseItemType[];
 
 		try {
 				const response = await fetch(url);
+				/* Annotate the data variable when it's type cannot be inferred
+					
+				Here we explicitly type the variable with our custom responseItemType
+				which cast the conversion's return value as an array of responseItemType
+
+				*/ 
 				data = (await response.json()) as responseItemType[];
 		} catch (error) {
 						return error;
